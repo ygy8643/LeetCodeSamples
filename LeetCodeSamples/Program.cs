3dd1564656b60fff944d8ddb3534d65a410b9318
IDH303 <IDH303@PC3854>
2018-02-14 16:04:35 +0900

    Add project files.

---
+++ LeetCodeSamples/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var results = TwoSum(new int[] { 3, 2, 4 }, 6);
+
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            
+
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                dic.Add(nums[i], i);
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index2;
+
+                if (dic.TryGetValue(target - nums[i], out index2) && index2 !=i)
+                {
+                    return new int[] { i, index2 };
+                }
+            }
+
+            throw new Exception("No solution");
+
+        }
+    }
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoSum
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// Using force loop.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSum1(int[] nums, int target)
+        {
+            int[] result = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                        return result;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Use Dictionary instead of using for loop.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSum2(int[] nums, int target)
+        {
+            var dic = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                dic.Add(nums[i], i);
+                var index2 = 0;
+
+                if (dic.TryGetValue(target - nums[i], out index2) && index2 != i)
+                    return new[] {index2, i};
+            }
+
+            throw new Exception("No solution");
+        }
+    }
+}
